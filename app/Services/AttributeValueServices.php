5ea737f4e907845b83e283f6bc953616ead3cbf3
mostafaEdreas <mostafa.fahmy205@gmail.com>
2024-12-01 00:39:08 +0200

    add  3

--- app/Services/AttributeValueServices.php
+++ app/Services/AttributeValueServices.php
@@ -7,9 +7,7 @@ class AttributeValueServices {
     public function store(array $data){
         return AttributeValue::craete($data);
     }
-    public function update($id ,array $data){
-        return AttributeValue::find($id)?->update($data);
-    }
+ 
 
     public function destory($id){
         return AttributeValue::find($id)?->delete();
